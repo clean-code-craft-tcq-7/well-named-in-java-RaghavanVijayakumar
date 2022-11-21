fe239539e4c13e6c194a526a2db393e2ea494371
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-11-21 11:15:47 +0530

    Create ColorPairTest.java

---
+++ colorcoder/ColorPairTest.java
@@ -0,0 +1,18 @@
+package colorcoder;
+
+public class ColorPairTest {
+
+	static void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
+		ColorPair colorPair = ColorPair.GetColorFromPairNumber(pairNumber);
+		System.out.println("Got pair " + colorPair.ToString());
+		assert (colorPair.getMajor() == expectedMajor);
+		assert (colorPair.getMinor() == expectedMinor);
+	}
+
+	static void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) {
+		int pairNumber = ColorPair.GetPairNumberFromColor(major, minor);
+		System.out.println("Got pair number " + pairNumber);
+		assert (pairNumber == expectedPairNumber);
+	}
+
+}
