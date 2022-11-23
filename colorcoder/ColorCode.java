0fc9c1cd0f9a52e2ef3730035b11d20484bdd195
RaghavanVijayakumar <115541217+RaghavanVijayakumar@users.noreply.github.com>
2022-11-23 14:51:25 +0530

    Update ColorCode.java

--- colorcoder/ColorCode.java
+++ colorcoder/ColorCode.java
@@ -3,19 +3,20 @@ package colorcoder;
 public class ColorCode {
 	static int GetColorCodeFromColor(MajorColor major, MinorColor minor) {
 		int Colorcode = 0;
+		int MinorColorIndexCount = ColorConstants.numberOfMinorColors - 1;
 		Colorcode = major.getIndex() * 1 + minor.getIndex() + 1;
 		switch (major.getIndex()) {
 		case 1:
-			Colorcode = Colorcode + 4;
+			Colorcode = Colorcode + MinorColorIndexCount;
 			break;
 		case 2:
-			Colorcode = Colorcode + 8;
+			Colorcode = Colorcode + MinorColorIndexCount * 2;
 			break;
 		case 3:
-			Colorcode = Colorcode + 12;
+			Colorcode = Colorcode + MinorColorIndexCount * 3;
 			break;
 		case 4:
-			Colorcode = Colorcode + 16;
+			Colorcode = Colorcode + MinorColorIndexCount * 4;
 			break;
 		}
 		return Colorcode;
